c30c902c5667dbd4bd4c19019684079fff75ed39
Christianchesire <christian.chesire@strathmore.edu>
2021-06-26 17:24:07 -0400

    Build Message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+# User.create(username: "Chistian", password: "password")
+# User.create(username: "Johnsnow", password: "password")
+# User.create(username: "Arya", password: "password")
+# User.create(username: "Frodo", password: "password")
+# User.create(username: "Gandalf", password: "password")
 
-User.create(username: "Chistian", password: "password")
-User.create(username: "Johnsnow", password: "password")
-User.create(username: "Arya", password: "password")
-User.create(username: "Frodo", password: "password")
-User.create(username: "Gandalf", password: "password")
+Message.create(body:"Hello everyone", user:user_id(1))
+Message.create(body:"Hey new guy", user:user_id(2))
+Message.create(body:"I'm chillin like..", user:user_id(3))
+Message.create(body:"Hi I'm a new me..", user:user_id(4))

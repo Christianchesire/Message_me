5b2800ad840981f62f44a65aa0a96812a4b4ca07
Christianchesire <christian.chesire@strathmore.edu>
2021-07-06 03:17:55 -0400

    Restrict views at controller layer

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,7 @@
 class ChatroomController < ApplicationController
+  # restrict user to be logged_in to access chatroom 
+  before_action :require_user
+
   def index
     @messages = Message.all
   end
